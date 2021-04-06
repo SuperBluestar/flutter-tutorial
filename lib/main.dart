f0dae32ddcb6680c4fe3ef1aacdc47bd2a6c2362
Lucy <snow-mice116@hotmail.com>
2021-04-06 22:52:51 +0900

    first app complete

--- lib/main.dart
+++ lib/main.dart
@@ -53,27 +53,28 @@ class MyApp extends StatelessWidget {
       ),
     );
 
-    return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter layout demo'),
+    Widget content = ListView(
+      children: [
+        Image.asset(
+          'images/lake.png',
+          width: 600,
+          height: 240,
+          fit: BoxFit.cover,
         ),
-        body: ListView(
-          children: [
-            Image.asset(
-              'images/lake.png',
-              width: 600,
-              height: 240,
-              fit: BoxFit.cover,
-            ),
-            titleSection,
-            buttonSection,
-            textSection
-          ],
-        ),
-      ),
+        titleSection,
+        buttonSection,
+        textSection
+      ],
     );
+
+    return MaterialApp(
+        title: 'Flutter layout demo',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter layout demo'),
+          ),
+          body: content,
+        ));
   }
 
   Widget titleSection = Container(
